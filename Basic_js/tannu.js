25a12fafe10349d674833bc23fc16f8cf91c772c
happysonkar <happy.sonkar90@gmail.com>
2023-11-29 17:38:32 +0530

    first commit

---
+++ Basic_js/tannu.js
@@ -0,0 +1,30 @@
+console.log('hello world');
+// console.log(sum=5+5)
+// console.log(sum=5+'25')
+// const names= ["Saab", "Volvo", "BMW", "Range Rover"];
+//num.push() to add something.
+// console.log(num.push("Audi"));
+//num.unshift(0)
+// console.log (num.unshift("jaguar"))
+//num .pop()to remove something.
+// console.log(num.shift())
+// names.indexOf('Fortuner')
+// console.log(names.indexOf('Volvo'))
+// console.log (names.lastIndexOf('Range Rover',4))
+// let channels=[{name: 'Sonkar kitchen',
+// subscriber:20000},
+// {name: 'Dharmender vlogs',
+// subscriber:2000},
+// {name: 'Sonkar Bhajans',
+// subscriber:200}];
+// console.log(channels.find(function(element){
+//     return element.name === 'Dharmender vlogs'
+// }))
+
+// let cars1=["Saab", "Volvo", "BMW"]
+// let cars2=["Range rover", "Mercedes", "Legender"]
+// let cars3= cars1.concat(cars2)
+// console.log(cars3.slice(0,3))
+//spread operator
+// let cars3=[...cars1,...cars2]
+// console.log(cars3)
